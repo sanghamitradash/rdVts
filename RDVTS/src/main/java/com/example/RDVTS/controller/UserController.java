89215207dbd69afa3f74a32475e11d4d8e43fdce
swarupbahinipati <Swarup.bahinipati@orsac.team>
2022-10-10 16:25:02 +0530

    initial commit

--- RDVTS/src/main/java/com/example/RDVTS/controller/UserController.java
+++ RDVTS/src/main/java/com/example/RDVTS/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.example.RDVTS.controller;
 
+import com.example.RDVTS.dto.RDVTSResponse;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin("*")
 @RequestMapping("/api/v1/users")
 public class UserController {
+
+    @GetMapping("/getAllUserDetails")
+    public RDVTSResponse getAllUserDetails() {
+        return null;
+    }
 }
---
+++ RDVTS/src/main/java/com/example/RDVTS/model/UserModel.java
@@ -0,0 +1,9 @@
+package com.example.RDVTS.model;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserModel {
+}
