e2febcbd2f2a93316dfec27efd0fd32d8e39bb1a
swarupbahinipati <Swarup.bahinipati@orsac.team>
2022-10-10 17:07:35 +0530

    commit 10022022 - 0445

--- RDVTS/src/main/java/com/example/RDVTS/controller/UserController.java
+++ RDVTS/src/main/java/com/example/RDVTS/controller/UserController.java
@@ -11,8 +11,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/users")
 public class UserController {
 
-    @GetMapping("/getAllUserDetails")
-    public RDVTSResponse getAllUserDetails() {
-        return null;
+
     }
-}
