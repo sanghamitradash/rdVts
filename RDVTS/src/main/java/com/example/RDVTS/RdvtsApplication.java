bfdbaa8416853d804291cbc18b930f3eedea1219
ORSAC <satyabratabehera995@gmail.com>
2022-10-10 16:03:59 +0530

    initial push

---
+++ RDVTS/src/main/java/com/example/RDVTS/RdvtsApplication.java
@@ -0,0 +1,13 @@
+package com.example.RDVTS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdvtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RdvtsApplication.class, args);
+	}
+
+}
---
+++ RDVTS/src/main/java/com/example/RDVTS/dto/RDVTSResponse.java
@@ -0,0 +1,16 @@
+package com.example.RDVTS.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.ResponseEntity;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RDVTSResponse {
+
+        private int status;
+        private ResponseEntity statusCode;
+        private String message;
+        public Object data ;
+}
