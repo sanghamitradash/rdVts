fd73fabd4307dc58309ac1e3fb080c6356f7b174
subhadarshini <subhadarshinisahoo7890@gmail.com>
2022-10-10 17:20:29 -0700

    entity creation 5:20

--- RDVTS/src/main/java/com/example/RDVTS/entities/UserPasswordMaster.java
+++ RDVTS/src/main/java/com/example/RDVTS/entities/UserPasswordMaster.java
@@ -3,7 +3,10 @@ package com.example.RDVTS.entities;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import javax.persistence.*;
 import java.util.Date;
 
 @Data
@@ -11,13 +14,34 @@ import java.util.Date;
 @NoArgsConstructor
 public class UserPasswordMaster {
 
+    @Id
+    @SequenceGenerator(name = "user_password_m_seq", sequenceName = "user_password_m_id_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_password_m_seq")
+    @Column(name = "id")
     private Integer id;
-    private Integer userId;
-    private String password;
-    private Boolean isActive;
-    private Integer createdBy;
-    private Date createdOn;
-    private Integer updatedBy;
-    private Date updatedOn;
 
+    @Column(name = "userId")
+    private Integer userId;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name="is_active",nullable = false)
+    private Boolean isActive=true;
+
+    @Column(name = "createdBy")
+    private Integer createdBy;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_on")
+    @CreationTimestamp
+    private Date createdOn;
+
+    @Column(name = "updatedBy")
+    private Integer updatedBy;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_on")
+    @UpdateTimestamp
+    private Date updatedOn;
 }
