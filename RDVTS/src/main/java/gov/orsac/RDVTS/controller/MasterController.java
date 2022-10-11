e53d2731381c63cd532996b9211020504409843c
swarupbahinipati <Swarup.bahinipati@orsac.team>
2022-10-11 13:22:04 +0530

    commit 10102022 - 0800

--- RDVTS/src/main/java/gov/orsac/RDVTS/controller/MasterController.java
+++ RDVTS/src/main/java/gov/orsac/RDVTS/controller/MasterController.java
@@ -2,6 +2,7 @@ package gov.orsac.RDVTS.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import gov.orsac.RDVTS.dto.DesignationDto;
 import gov.orsac.RDVTS.dto.RDVTSResponse;
 import gov.orsac.RDVTS.entities.DesignationEntity;
 import gov.orsac.RDVTS.service.DesignationService;
@@ -9,8 +10,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -22,7 +26,7 @@ public class MasterController {
 
     ObjectMapper objectMapper = new ObjectMapper();
     @PostMapping("/saveDesignation")
-    public RDVTSResponse saveTender(@RequestBody DesignationEntity designationEntity) throws JsonProcessingException {
+    public RDVTSResponse saveDesignation(@RequestBody DesignationEntity designationEntity) throws JsonProcessingException {
         RDVTSResponse rdvtsResponse = new RDVTSResponse();
         Map<String, Object> result = new HashMap<>();
         try{
@@ -41,10 +45,83 @@ public class MasterController {
         return rdvtsResponse;
     }
 
-    @GetMapping("/getAllDesignation")
-    public RDVTSResponse getAllDesignation(){
-        return designationService.getAllDesignation();
+//    @GetMapping("/getAllDesignation")
+//    public RDVTSResponse getAllDesignation(){
+//        return designationService.getAllDesignation();
+//    }
+@PostMapping("/getAllDesignationByUserLevelId")
+public RDVTSResponse getAllDesignationByUserLevelId(@RequestParam int userLevelId) {
+    RDVTSResponse response = new RDVTSResponse();
+    Map<String, Object> result = new HashMap<>();
+    try {
+        //List<Integer> userIdList = new ArrayList<>();
+        List<DesignationEntity> designationList = designationService.getAllDesignationByUserLevelId(userLevelId);
+        if (!designationList.isEmpty() && designationList.size() > 0) {
+            result.put("designationList", designationList);
+            response.setData(result);
+            response.setStatus(1);
+            response.setStatusCode(new ResponseEntity<>(HttpStatus.OK));
+        } else {
+            result.put("designationList", designationList);
+            response.setData(result);
+            response.setStatus(1);
+            response.setStatusCode(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+            response.setMessage("Record not found.");
+        }
+    } catch (Exception e) {
+        response = new RDVTSResponse(0,
+                new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR),
+                e.getMessage(),
+                result);
+    }
+    return response;
+
+}
+
+    @PostMapping("/updateDesignation")
+    public RDVTSResponse updateDesignation(@RequestParam int id,
+                                           @RequestParam(name = "data") String data) {
+        RDVTSResponse rdvtsResponse = new RDVTSResponse();
+        Map<String, Object> result = new HashMap<>();
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            DesignationDto updateDesignationDto = mapper.readValue(data, DesignationDto.class);
+            DesignationEntity updateDesignationEntity = designationService.updateDesignation(id, updateDesignationDto);
+            rdvtsResponse.setData(result);
+            rdvtsResponse.setStatus(1);
+            rdvtsResponse.setStatusCode(new ResponseEntity<>(HttpStatus.OK));
+            rdvtsResponse.setMessage("Designation Updated Successfully");
+        } catch (Exception e) {
+            rdvtsResponse = new RDVTSResponse(0,
+                    new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR),
+                    e.getMessage(),
+                    result);
+        }
+        return rdvtsResponse;
     }
 
+    @PostMapping("/activateAndDeactivateDesignation")
+    public RDVTSResponse activateAndDeactivateDesignation(@RequestParam Integer id) {
+        RDVTSResponse response = new RDVTSResponse();
+        Map<String, Object> result = new HashMap<>();
+        try {
+            Boolean res = designationService.activateAndDeactivateDesignation(id);
+            if (res) {
+                response.setData(result);
+                response.setStatus(1);
+                response.setMessage("Designation Status Changed Successfully");
+                response.setStatusCode(new ResponseEntity<>(HttpStatus.OK));
+            } else {
+                response.setStatus(0);
+                response.setMessage("Something went wrong");
+                response.setStatusCode(new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR));
+            }
+        } catch (Exception e) {
+            response = new RDVTSResponse(0,
+                    new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR),
+                    e.getMessage(), result);
+        }
+        return response;
+    }
 
 }
--- RDVTS/src/main/java/gov/orsac/RDVTS/service/DesignationService.java
+++ RDVTS/src/main/java/gov/orsac/RDVTS/service/DesignationService.java
@@ -1,12 +1,21 @@
 package gov.orsac.RDVTS.service;
 
+import gov.orsac.RDVTS.dto.DesignationDto;
 import gov.orsac.RDVTS.dto.RDVTSResponse;
 import gov.orsac.RDVTS.entities.DesignationEntity;
+
+import java.util.List;
 
 public interface DesignationService {
 
     DesignationEntity saveDesignation (DesignationEntity designationEntity);
 
-    RDVTSResponse getAllDesignation();
+    //RDVTSResponse getAllDesignation();
+
+    List<DesignationEntity> getAllDesignationByUserLevelId(int userLevelId);
+
+    DesignationEntity updateDesignation (int id, DesignationDto designationDto);
+
+    Boolean activateAndDeactivateDesignation(Integer id);
 
 }
