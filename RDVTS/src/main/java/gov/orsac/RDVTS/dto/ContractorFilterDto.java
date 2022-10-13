a409efa41457ebb10d87c354dc460830f85e5710
bhagyashreepalai <bhagyashree.palai@orsacgeoict.in>
2022-10-13 12:05:22 +0530

    CONTRACT LISTING CHANGES, ADD DEVICE CHANGES 13/10/2022

---
+++ RDVTS/src/main/java/gov/orsac/RDVTS/dto/ContractorFilterDto.java
@@ -0,0 +1,16 @@
+package gov.orsac.RDVTS.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ContractorFilterDto {
+
+    Integer id;
+    Integer userId;
+    Integer gContractorId;
+}
+
