d6a74144f0a0469747ab5703d1a11bce3dcf9e35
bhagyashreepalai <bhagyashree.palai@orsacgeoict.in>
2022-10-14 12:58:48 +0530

    ROAD and STATE 14/10/2022(12.58PM)

---
+++ RDVTS/src/main/java/gov/orsac/RDVTS/dto/StateDto.java
@@ -0,0 +1,17 @@
+package gov.orsac.RDVTS.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class StateDto {
+
+    private Integer id;
+
+    private String stateName;
+
+    private Boolean isActive;
+}
