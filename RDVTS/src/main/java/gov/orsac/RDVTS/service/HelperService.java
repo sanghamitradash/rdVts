b3616b51756f8bc58257e4135944ed097e8ce728
ORSAC <satyabratabehera995@gmail.com>
2022-10-14 10:52:55 +0530

    Helper Function

---
+++ RDVTS/src/main/java/gov/orsac/RDVTS/service/HelperService.java
@@ -0,0 +1,4 @@
+package gov.orsac.RDVTS.service;
+
+public interface HelperService {
+}
---
+++ RDVTS/src/main/java/gov/orsac/RDVTS/serviceImpl/HelperServiceImpl.java
@@ -0,0 +1,6 @@
+package gov.orsac.RDVTS.serviceImpl;
+
+import gov.orsac.RDVTS.service.HelperService;
+
+public class HelperServiceImpl implements HelperService {
+}
