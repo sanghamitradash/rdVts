45bb5d780e8e63cb8b95c147ae5029ff63d71aa8
bhagyashreepalai <bhagyashree.palai@orsacgeoict.in>
2022-10-17 16:16:28 +0530

    Device CHANGES 17/10/2022(04.15PM)

--- RDVTS/src/main/java/gov/orsac/RDVTS/repository/DeviceRepository.java
+++ RDVTS/src/main/java/gov/orsac/RDVTS/repository/DeviceRepository.java
@@ -4,4 +4,7 @@ import gov.orsac.RDVTS.entities.DeviceEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DeviceRepository extends JpaRepository<DeviceEntity,Integer> {
+    Boolean existsByImeiNo1(Long imeiNo1);
+
+    Boolean existsByImeiNo2(Long imeiNo2);
 }
