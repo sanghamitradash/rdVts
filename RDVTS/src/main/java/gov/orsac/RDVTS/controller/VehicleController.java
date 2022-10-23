c10d4da723edd3e6816aff99720e83e581c6ce8a
ORSAC <satyabratabehera995@gmail.com>
2022-10-23 19:33:24 +0530

    Vehicle List

--- RDVTS/src/main/java/gov/orsac/RDVTS/controller/VehicleController.java
+++ RDVTS/src/main/java/gov/orsac/RDVTS/controller/VehicleController.java
@@ -278,7 +278,7 @@ public class VehicleController {
             for (int i = 0; i < vehicleList.size(); i++) {
                 start1 = start1 + 1;
                 vehicleList.get(i).setSlNo(start1);
-                boolean device = vehicleRepositoryImpl.getDeviceAssignedOrNot(vehicleList.get(i).getId());
+             /*   boolean device = vehicleRepositoryImpl.getDeviceAssignedOrNot(vehicleList.get(i).getId());
                 if (vehicleList.get(i).getDeviceId() != null) {
                     DeviceDto deviceData = deviceRepositoryImpl.getDeviceByIdForTracking(vehicleList.get(i).getDeviceId());
                     boolean trackingVehicle = vehicleRepositoryImpl.getTrackingLiveOrNot(deviceData.getImeiNo1());
@@ -286,9 +286,9 @@ public class VehicleController {
                 }
                 boolean activity = vehicleRepositoryImpl.getActivityAssignOrNot(vehicleList.get(i).getId());
                 vehicleList.get(i).setDeviceAssigned(device);
-                vehicleList.get(i).setActivityAssigned(activity);
+                vehicleList.get(i).setActivityAssigned(activity);*/
             }
-
+/*
 
             if (vehicleList != null && vehicleList.size() > 0) {
                 for (VehicleMasterDto master : vehicleList) {
@@ -325,14 +325,15 @@ public class VehicleController {
                     }
 
                 }
-            }
+            }*/
 
-            response.setData(vehicleActivityList);
+            response.setData(vehicleList);
             // response.setData(vehicleActivityList);
             response.setMessage("Vehicle List");
             response.setStatus(1);
             response.setDraw(draw);
-            response.setRecordsFiltered(Long.valueOf(vehicleListPage.getNumberOfElements()));
+           // response.setRecordsFiltered(Long.valueOf(vehicleListPage.getNumberOfElements()));
+            response.setRecordsFiltered(vehicleListPage.getTotalElements());
             response.setRecordsTotal(vehicleListPage.getTotalElements());
             response.setStatusCode(new ResponseEntity<>(HttpStatus.OK));
         } catch (Exception e) {
@@ -377,7 +378,7 @@ public class VehicleController {
             result.put("saveVehicleMapping", saveVehicleMapping);
             response.setData(result);
             response.setStatus(1);
-            response.setMessage("Assign Vehicle Device Created Successfully");
+            response.setMessage("Assign Vehicle Device Updated Successfully");
             response.setStatusCode(new ResponseEntity<>(HttpStatus.OK));
             /*        }
                     else {
