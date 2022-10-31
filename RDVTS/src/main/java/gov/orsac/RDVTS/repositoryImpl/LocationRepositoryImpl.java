726f5cf310f4796eeb3d35458acf72b4b611d0d7
Soubhagyapradhan49 <Soubhagyapradhan49@gmail.com>
2022-10-31 12:23:06 +0530

    changes in user an=nd location

--- RDVTS/src/main/java/gov/orsac/RDVTS/repositoryImpl/LocationRepositoryImpl.java
+++ RDVTS/src/main/java/gov/orsac/RDVTS/repositoryImpl/LocationRepositoryImpl.java
@@ -229,13 +229,13 @@ public class LocationRepositoryImpl {
                 sqlParam.addValue("deactivationDate", deviceVehicleDeactivationDate);
             }
 
-            qry += " order by date_time desc LIMIT 1";
+            qry += " and date(date_time)=date(now())   order by date_time desc LIMIT 1";
 
 
             return namedJdbc.query(qry, sqlParam, new BeanPropertyRowMapper<>(VtuLocationDto.class));
         } else {
             String qry = "SELECT id, firmware_version, packet_type, alert_id, packet_status, imei, vehicle_reg, gps_fix, date_time, latitude, latitude_dir, longitude, longitude_dir, speed, heading, no_of_satellites, altitude, pdop, hdop, network_operator_name, ignition, main_power_status, main_input_voltage, internal_battery_voltage, emergency_status, tamper_alert, gsm_signal_strength, mcc, mnc, lac, cell_id, lac1, cell_id1, cell_id_sig1, lac2, cell_id2, cell_id_sig2, lac3, cell_id3, cell_id_sig3, lac4, cell_id4, cell_id_sig4, digital_input1, digital_input2, digital_input3, digital_input4, digital_output_1, digital_output_2, frame_number, checksum, odo_meter, geofence_id, is_active, created_by, created_on, updated_by, updated_on\n" +
-                    "\tFROM rdvts_oltp.vtu_location where is_active=true ";
+                    "\tFROM rdvts_oltp.vtu_location where is_active=true   ";
 
             qry += " and imei =:imei1 and gps_fix::numeric =1 ";
             sqlParam.addValue("imei1", imei1);
@@ -249,7 +249,7 @@ public class LocationRepositoryImpl {
                 sqlParam.addValue("createdOn", deviceVehicleCreatedOn);
                 sqlParam.addValue("deactivationDate", deviceVehicleDeactivationDate);
             }
-            qry += " order by date_time desc LIMIT 1";
+            qry += " and date(date_time)=date(now()) order by date_time desc LIMIT 1";
 
 
 
@@ -388,7 +388,7 @@ public class LocationRepositoryImpl {
                 sqlParam.addValue("createdOn", deviceVehicleCreatedOn);
                 sqlParam.addValue("deactivationDate", deviceVehicleDeactivationDate);
             }
-            qry += "and imei=:imei2 and date(date_time)=date(now()) and gps_fix::numeric =1 and date(date_time)=date(now())  order by date_time desc )";
+            qry += "and imei=:imei2 and date(date_time)=date(now()) and gps_fix::numeric =1 order by date_time desc )";
             sqlParam.addValue("imei2", imei2);
 
             return namedJdbc.queryForObject(qry, sqlParam, Double.class);
