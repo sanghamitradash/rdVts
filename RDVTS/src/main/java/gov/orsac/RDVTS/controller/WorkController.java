cd5e168b34b3af94ce380c819bf646f2d46d408e
Soubhagyapradhan49 <Soubhagyapradhan49@gmail.com>
2022-10-31 11:29:19 +0530

    changes in user an=nd location

--- RDVTS/src/main/java/gov/orsac/RDVTS/controller/WorkController.java
+++ RDVTS/src/main/java/gov/orsac/RDVTS/controller/WorkController.java
@@ -119,7 +119,8 @@ public class WorkController {
             Date vehicleendDate = null;
             Double todayDistance=0.0;
             Double totalDistance = 0.0;
-            Double totalSpeed=0.0;
+            Double totalSpeedWork=0.0;
+            Double avgSpeedToday=0.0;
             Integer totalActiveVehicle=0;
 
             //distance and speed API
@@ -143,9 +144,9 @@ public class WorkController {
                                 int i=0;
                                 for (VtuLocationDto vtuobj : vtuLocationDto) {
                                     i++;
-                                    totalSpeed+= Double.parseDouble(vtuobj.getSpeed()) ;
+                                    totalSpeedWork+= Double.parseDouble(vtuobj.getSpeed()) ;
                                 }
-                                totalSpeed=totalSpeed/i;
+                                totalSpeedWork=totalSpeedWork/i;
                             }
                         }
                     }
@@ -156,22 +157,22 @@ public class WorkController {
             int totalVehicleCount=vehicleService.getvehicleCountByWorkId(id);
             Double avgDistance;
             if(Double.isNaN(totalDistance/totalVehicleCount)){
-                avgDistance=null;
+                avgDistance=0.0;
             }
             else {
                  avgDistance=totalDistance/totalVehicleCount;
             }
-            //Double todayAvgDistance=todayDistance/totalVehicleCount;
-            Double avgSpeed;
-            if(Double.isNaN(totalSpeed/totalVehicleCount)){
-                avgSpeed=null;
+            Double todayAvgDistance=todayDistance/totalVehicleCount;
+            Double avgSpeedWork;
+            if(Double.isNaN(totalSpeedWork/totalVehicleCount)){
+                avgSpeedWork=0.0;
             }
             else {
-                avgSpeed=totalSpeed/totalVehicleCount;
+                avgSpeedWork=totalSpeedWork/totalVehicleCount;
             }
             Double percentageOfTotalActiveVehicle;
             if(Double.isNaN((totalActiveVehicle)/Double.valueOf(totalVehicleCount)*100)){
-                percentageOfTotalActiveVehicle=null;
+                percentageOfTotalActiveVehicle=0.0;
             }
             else {
                 percentageOfTotalActiveVehicle = Double.valueOf(totalActiveVehicle)/Double.valueOf(totalVehicleCount)*100 ;
@@ -183,8 +184,9 @@ public class WorkController {
             location.setAvgDistanceTravelled(avgDistance);
             location.setDistanceTravelledToday(todayDistance);
             //location.setAvgDistanceTravelled(todayAvgDistance);
-            location.setSpeed(totalSpeed);
-            location.setAvgSpeed(avgSpeed);
+            //location.setSpeed(totalSpeed);
+            location.setAvgSpeedToday(avgSpeedToday);//Toady
+            location.setAvgSpeedWork(avgSpeedWork);
             location.setTotalAlertToday(1);
             location.setTotalAlertWork(1);
             location.setTotalVehicleActive(totalActiveVehicle);
