7eff1f3ec6d82ab562906c75280dd1780e8d7749
subhadarshini <subhadarshinisahoo7890@gmail.com>
2022-11-15 17:21:11 +0530

    changes API 5:21 15/11/22

--- RDVTS/src/main/java/gov/orsac/RDVTS/repositoryImpl/AlertRepositoryImpl.java
+++ RDVTS/src/main/java/gov/orsac/RDVTS/repositoryImpl/AlertRepositoryImpl.java
@@ -15,10 +15,10 @@ public class AlertRepositoryImpl {
 
     public Integer getTotalAlertToday() {
         MapSqlParameterSource sqlParam = new MapSqlParameterSource();
-        String qry = " select distinct id, imei,alert_type_id from rdvts_oltp.alert_data where imei in (select imei_no_1 from rdvts_oltp.device_m where id in " +
+        String qry = " select distinct count(id) from rdvts_oltp.alert_data where imei in (select imei_no_1 from rdvts_oltp.device_m where id in " +
                 " (select device_id from rdvts_oltp.vehicle_device_mapping where vehicle_id in " +
                 " (select vehicle_id from rdvts_oltp.vehicle_activity_mapping where activity_id in " +
-                " (select activity_id from rdvts_oltp.activity_work_mapping where work_id in (select id from rdvts_oltp.work_m))))) " +
+                " (select activity_id from rdvts_oltp.activity_work_mapping where work_id in (:workId))))) " +
                 " and date(gps_dtm)=date(now()) and is_active=true " ;
         return namedJdbc.queryForObject(qry,sqlParam,Integer.class);
     }
