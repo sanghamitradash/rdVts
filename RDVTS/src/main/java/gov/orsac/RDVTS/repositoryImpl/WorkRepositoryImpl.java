a60bd3461ed585d8627e81bc058003f2748586d7
Santosh-Parida <paridasantosh040@gmail.com>
2023-07-21 13:34:12 +0530

    dt: 21 jul 2023 1:33pm

--- RDVTS/src/main/java/gov/orsac/RDVTS/repositoryImpl/WorkRepositoryImpl.java
+++ RDVTS/src/main/java/gov/orsac/RDVTS/repositoryImpl/WorkRepositoryImpl.java
@@ -312,7 +312,7 @@ public class WorkRepositoryImpl {
     public List<GeoConstructionDto> getPackageDD(Integer userId, Integer piuId) {
         MapSqlParameterSource sqlParam = new MapSqlParameterSource();
         String qry = "select distinct pm.id as packageId ,pm.package_no as packageName from rdvts_oltp.package_m as pm " +
-                "left join rdvts_oltp.geo_mapping as gm on gm.piu_id=pm.id and gm.is_active=true " +
+                "left join rdvts_oltp.geo_mapping as gm on gm.package_id=pm.id and gm.is_active=true " +
                 " where pm.is_active = true ";
         if(piuId != null && piuId > 0){
             qry += " and gm.piu_id=:piuId ";
